042a6f82a6bf9de99b6017fe89eb47c2012ba66e
jesse <jessedp@gmail.com>
2020-05-17 22:06:59 -0400

    get this working, add version slickness

--- setup.py
+++ setup.py
@@ -1,26 +1,41 @@
-import setuptools
+import io
+import ast
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="pihole5-list-tool",
-    version="0.1.0",
-    author="jesse",
-    author_email="jessedp@gmail.com",
-    description="Tool to add block/ad/ignore lists to pi-hole5",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jessedp/pihole5-list-tool",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-        'PyInquirer',
-        'ansicolors'
-    ]
-)
+def version():
+    """Return version string."""
+    with io.open('ph5lt.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+with io.open("README.md") as readme:
+    setup(
+        name="pihole5-list-tool",
+        version=version(),
+        author="jesse",
+        author_email="jessedp@gmail.com",
+        description="Tool to add block/ad/ignore lists to pi-hole5",
+        long_description=readme.read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/jessedp/pihole5-list-tool",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Topic :: Utilities",
+            "Topic :: Internet :: Name Service (DNS)",
+        ],
+        keywords='pihole, pi-hole, blacklist, blocklist, adlist',
+        python_requires='>=3.6',
+        install_requires=[
+            'PyInquirer',
+            'ansicolors'
+        ],
+        py_modules=['ph5lt'],
+        entry_points={
+            'console_scripts': ['pihole5-list-tool = ph5lt:main']
+        },
+    )
