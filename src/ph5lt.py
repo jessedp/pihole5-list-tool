6f37ee258b9d0dd30b35737e6cf29e3c316eda52
jesse <jessedp@gmail.com>
2020-05-22 19:57:49 -0400

    pylint changes

--- src/ph5lt.py
+++ src/ph5lt.py
@@ -28,19 +28,18 @@
 
 
 import os
-from sys import exit
+import sys
 from urllib.parse import urlparse
-import requests
 import sqlite3
+import requests
 
 
 from colors import color
-from PyInquirer import prompt, Validator, ValidationError
+
 
 import constants
-
-# from pprint import pprint
-
+import inquirer
+import utils
 
 __version__ = '0.3.3'
 
@@ -63,7 +62,7 @@ blackLists = {
 whiteLists = {
     constants.W_ANUDEEP_WHITE: {
         'url': 'https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt',
-        'comment': 'AndeepND | Whitelist Only - Domains that are safe to whitelist i.e does not contain any tracking or advertising sites. This fixes many problems like YouTube watch history, videos on news sites and so on.',
+        'comment': "AndeepND | Whitelist Only - Domains that are safe to whitelist i.e does not contain any tracking or advertising sites. This fixes many problems like YouTube watch history, videos on news sites and so on.",
     },
     constants.W_ANUDEEP_REFERRAL: {
         'url': 'https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt',
@@ -77,263 +76,109 @@ whiteLists = {
 
 
 def main():
+    """main method"""
     try:
         print(color('\n    ┌──────────────────────────────────────────┐', fg='#b61042'))
         print(color('    │       ', fg='#b61042') +
               color(f'π-hole 5 list tool  v.{__version__}', '#FFF') + color('        │    ', fg='#b61042'))
         print(color('    └──────────────────────────────────────────┘\n', fg='#b61042'))
 
-        result = askDb()
-        dbFile = result['gravitydb']
+        result = inquirer.askDb()
+        db_file = result['gravitydb']
 
-        result = askListType()
-        type = result['listType']
-        result = askBlacklist()
+        result = inquirer.askListType()
+        list_type = result['listType']
+        result = inquirer.askBlacklist()
         source = result['source']
 
         # Get imports from somewher
-        importList = []
+        import_list = []
 
-        def validUrl(url):
+        def valid_url(url):
             parts = urlparse(url.strip())
             return parts.scheme and parts.netloc
 
-        def processLines(data, comment):
-            newData = []
+        def process_lines(data, comment):
+            new_data = []
             for line in data.strip().split("\n"):
                 line = line.strip()
                 if line == '':
                     continue
-                if not validUrl(line):
-                    warn(f'Skipping: {line}')
+                if not valid_url(line):
+                    utils.warn(f'Skipping: {line}')
                 else:
-                    newData.append({'url': line, 'comment': comment})
-            return newData
+                    new_data.append({'url': line, 'comment': comment})
+            return new_data
 
-        if config['source'] in blackLists:
-            urlSource = blackLists[source]
-            resp = requests.get(urlSource['url'])
-            importList = processLines(resp.text, urlSource['comment'])
+        if result['source'] in blackLists:
+            url_source = blackLists[source]
+            resp = requests.get(url_source['url'])
+            import_list = process_lines(resp.text, url_source['comment'])
 
-        if source == FILE:
-            choice = askImportFile()
-            file = choice['file']
-            f = open(file)
-            importList = processLines(f.read(), f'File: {file}')
+        if source == constants.FILE:
+            choice = inquirer.askImportFile()
+            fname = choice['file']
 
-        if source == PASTE:
-            choice = askPaste()
-            importList = processLines(choice['content'], 'Pasted content')
+            import_file = open(fname)
 
-        if len(importList) == 0:
-            die('No valid urls found, try again')
+            import_list = process_lines(import_file, f'File: {fname}')
 
-        choice = confirm(
-            f'Add {len(importList)} block lists to {dbFile}?')
+        if source == constants.PASTE:
+            choice = inquirer.askPaste()
+            import_list = process_lines(choice['content'], 'Pasted content')
+
+        if len(import_list) == 0:
+            utils.die('No valid urls found, try again')
+
+        choice = inquirer.confirm(
+            f'Add {len(import_list)} block lists to {db_file}?')
 
         if not choice['confirm']:
-            warn('Nothing changed. Bye!')
-            exit(0)
+            utils.warn('Nothing changed. Bye!')
+            sys.exit(0)
 
-        conn = sqlite3.connect(config['gravitydb'])
-        db = conn.cursor()
+        conn = sqlite3.connect(db_file)
+        sqldb = conn.cursor()
         added = 0
         exists = 0
-        for item in importList:
-            db.execute(
-                "SELECT COUNT(*) FROM adlist WHERE address = ?", (item['url'],))
+        for item in import_list:
+            sqldb.execute(
+                "SELECT COUNT(*) FROM adlist WHERE address = ?",
+                (item['url'],))
 
-            cnt = db.fetchone()
+            cnt = sqldb.fetchone()
 
             if cnt[0] > 0:
                 exists += 1
             else:
                 added += 1
                 vals = (item['url'], item['comment'])
-                db.execute(
+                sqldb.execute(
                     'INSERT OR IGNORE INTO adlist (address, comment) VALUES (?,?)', vals)
                 conn.commit()
 
-        db.close()
+        sqldb.close()
         conn.close()
 
-        success(f'{added} block lists added! {exists} already existed.')
+        utils.success(f'{added} block lists added! {exists} already existed.')
 
-        choice = confirm('Update Gravity for immediate affect?')
+        choice = inquirer.confirm('Update Gravity for immediate affect?')
 
         if choice['confirm']:
             print()
             os.system('pihole -g')
         else:
-            info('Update Gravity through the web interface or by running:\n\t# pihole -g')
+            utils.info(
+                'Update Gravity through the web interface or by running:\n\t# pihole -g')
 
-            info('\n\tBye!')
+            utils.info('\n\tBye!')
 
     except (KeyboardInterrupt, KeyError):
-        exit(0)
-
-
-def warn(msg):
-    print(color(msg, fg='yellow'))
-
-
-def success(msg):
-    print(color(msg, fg='lime'))
-
-
-def info(msg):
-    print(color(msg, fg='#5DADE2'))
-
-
-def die(msg):
-    print(color(msg, fg='orangered'))
-    exit(-1)
-
-
-class ValidateEditor(Validator):
-    def validate(self, document):
-        if document.text and len(document.text.split('\n')) <= 1:
-            raise ValidationError(
-                message='Must be at least 1 line', cursor_position=len(document.text))
-
-
-def keyPrompt(questions):
-    resp = prompt(questions)
-    if len(resp) != len(questions):
-        raise KeyboardInterrupt
-    return resp
-
-
-def askDb():
-    questions = [
-        {
-            'name': 'gravitydb',
-                    'type': 'input',
-                    'default': constants.defaultDb,
-                    'message': 'Gravity Db to Update:',
-                    'validate': lambda answer: f'Please enter a valid file name or nothing for {defaultDb} {answer}.'
-            if answer.strip() != '' and not os.path.exists(answer) else True
-
-        }
-    ]
-
-    return keyPrompt(questions)
-
-
-def askListType():
-    questions = [
-        {
-            'name': 'listType',
-                    'type': 'list',
-                    'default': 'black',
-                    'message': 'Add Blacklists or Whitelist?',
-                    'choices': [
-                        {
-                            'name':
-                            'Blacklists',
-                            'value': constants.BLACKLIST,
-                            'short': 'Blacklists',
-                        },
-                        {
-                            'name':
-                            'Whitelists',
-                            'value': constants.WHITELIST,
-                            'short': 'Whitelists',
-                        },
-                    ],
-
-        }
-    ]
-
-    return keyPrompt(questions)
-
-
-def askBlacklist():
-    questions = [
-        {
-            'name': 'source',
-                    'type': 'list',
-                    'message': 'Where are the block lists coming from?',
-                    'choices': [
-                        {
-                            'name':
-                            'Firebog | Non-crossed lists: For when someone is usually around to whitelist falsely blocked sites',
-                            'value': constants.B_FIREBOG_NOCROSS,
-                            'short': 'Firebog (no cross)',
-                        },
-                        {
-                            'name':
-                            'Firebog | Ticked lists: For when installing Pi-hole where no one will be whitelisting falsely blocked sites',
-                            'value': constants.B_FIREBOG_TICKED,
-                            'short': 'Firebog (ticked)',
-                        },
-                        {
-                            'name': 'Firebog | All lists: For those who will always be around to whitelist falsely blocked sites',
-                            'value': constants.B_FIREBOG_ALL,
-                            'short': 'Firebog (all)',
-                        },
-                        {
-                            'name': 'File    | A file with urls of lists, 1 per line',
-                            'value': constants.FILE,
-                            'short': 'File',
-                        },
-                        {
-                            'name': 'Paste   | Paste urls of lists, 1 per line - opens editor, save, close',
-                            'value': constants.PASTE,
-                            'short': 'Paste',
-                        },
-                    ]
-        }
-    ]
-
-    return keyPrompt(questions)
-
-
-def askImportFile():
-    questions = [
-        {
-            'name': 'file',
-            'type': 'input',
-            'message': 'File to import',
-            'validate': lambda value: 'Please enter a valid file name.' if not os.path.exists(value) else True
-        }
-    ]
-
-    return keyPrompt(questions)
-
-
-def askPaste():
-    questions = [
-        {
-            'name': 'content',
-            'type': 'editor',
-            'message': 'Opening editor',
-            # lambda text: len(text.split('\n')) >= 1 or 'Must be at least 1 line',
-            'validate': ValidateEditor,
-            'eargs': {
-                'editor': 'default',
-                'ext': '.tmp'
-            },
-
-        }
-    ]
-    return keyPrompt(questions)
-
-
-def confirm(message):
-    questions = [
-        {
-            'name': 'confirm',
-            'type': 'confirm',
-            'message': message,
-            'default': 'y',
-        }
-    ]
-    return keyPrompt(questions)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
     try:
         main()
     except (KeyboardInterrupt, KeyError):
-        exit(0)
+        sys.exit(0)
