d2e408b71f8f4a79117b49d5a03f8794fa053df6
jesse <jessedp@gmail.com>
2020-05-22 21:10:07 -0400

    move process_lines and valid_url to utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,27 @@
 """Utils"""
 import sys
+from urllib.parse import urlparse
 from colors import color
+
+
+def valid_url(url):
+    """make sure we have a valid url"""
+    parts = urlparse(url.strip())
+    return parts.scheme and parts.netloc
+
+
+def process_lines(data, comment):
+    """massage the lines so we have good ones"""
+    new_data = []
+    for line in data.strip().split("\n"):
+        line = line.strip()
+        if line == '':
+            continue
+        if not valid_url(line):
+            warn(f'Skipping: {line}')
+        else:
+            new_data.append({'url': line, 'comment': comment})
+    return new_data
 
 
 def warn(msg):
